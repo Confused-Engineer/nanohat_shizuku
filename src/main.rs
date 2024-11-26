c2f68a772e9732a09371321aa8cba7d6dd939f90
Confused-Engineer <dpierce33100@gmail.com>
2024-11-25 20:59:21 -0500

    this is bare minimum to verify what i want

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::time::Duration;
 
 use gpio::{GpioIn, GpioOut};
 
-const K1: u8 = 0;
-const K2: u8 = 2;
-const K3: u8 = 3;
+const K1: u16 = 0;
+const K2: u16 = 2;
+const K3: u16 = 3;
 fn main() {
     std::thread::sleep(std::time::Duration::from_secs(5));
     println!("write hello wrld");
@@ -33,9 +33,9 @@ fn get_pins()  {
     let mut k2 = gpio::sysfs::SysFsGpioInput::open(K2).unwrap();
     let mut k3 = gpio::sysfs::SysFsGpioInput::open(K3).unwrap();
     
-    println!("K1: {}", k1.read_value().unwrap());
-    println!("K2: {}", k2.read_value().unwrap());
-    println!("K3: {}", k3.read_value().unwrap());
+    println!("K1: {:?}", k1.read_value().unwrap());
+    println!("K2: {:?}", k2.read_value().unwrap());
+    println!("K3: {:?}", k3.read_value().unwrap());
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
     
