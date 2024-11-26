225fc9c8eb72507ebb58eba4ef5c3b96cdb2cc3b
Confused-Engineer <dpierce33100@gmail.com>
2024-11-25 20:55:18 -0500

    pls dont use this against me

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
-use rppal::system::DeviceInfo;
+use gpio::{GpioIn, GpioOut};
 
 const K1: u8 = 0;
 const K2: u8 = 2;
 const K3: u8 = 3;
 fn main() {
     std::thread::sleep(std::time::Duration::from_secs(5));
+    println!("write hello wrld");
     hello_wrld();
+    println!("read pins");
     get_pins();
 }
 
@@ -20,25 +21,22 @@ fn main() {
 fn hello_wrld() -> OledResult {
     let mut oled = Oled::from_path("/dev/i2c-0")?;
     oled.init()?;
-    oled.put_string("Hello, world!")?;
+    oled.put_string("Hello, World!")?;
     Ok(())
 }
 
 
 
-fn get_pins() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+fn get_pins()  {
 
-    let mut k1 = Gpio::new()?.get(K1)?.into_input();
-    let mut k2 = Gpio::new()?.get(K2)?.into_input();
-    let mut k3 = Gpio::new()?.get(K3)?.into_input();
+    let mut k1 = gpio::sysfs::SysFsGpioInput::open(K1).unwrap();
+    let mut k2 = gpio::sysfs::SysFsGpioInput::open(K2).unwrap();
+    let mut k3 = gpio::sysfs::SysFsGpioInput::open(K3).unwrap();
     
-    println!("K1: {}", k1.read().to_string());
-    println!("K2: {}", k2.read().to_string());
-    println!("K3: {}", k3.read().to_string());
+    println!("K1: {}", k1.read_value().unwrap());
+    println!("K2: {}", k2.read_value().unwrap());
+    println!("K3: {}", k3.read_value().unwrap());
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
     
-
-    Ok(())
 }
