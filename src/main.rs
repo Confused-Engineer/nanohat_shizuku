8e40e1f535e314f6b85198da18835cd6ca980c95
Confused-Engineer <dpierce33100@gmail.com>
2024-11-27 20:56:53 -0500

    image test

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ impl NanoPi
                     if self.screen_refresh_required
                     {
                         //if let Ok(_) = self.screen.put_string("Start: k1: adb, k3: shutdown")
-                        if let Ok(_) = self.screen.draw_image(include_bytes!("../assets/screen_main.bmp"), 100)
+                        if let Ok(_) = self.screen.draw_image(include_bytes!("../assets/screen_main.bmp"), 0)
                         {
                             self.screen_refresh_required = false;
                         } else {
